a53cfc9db9e1339702003cbd27f613c47ef62bfd
8borane8 <74107604+8borane8@users.noreply.github.com>
2024-06-26 17:21:22 +0200

    fix

--- src/web/HttpServer.ts
+++ src/web/HttpServer.ts
@@ -32,7 +32,15 @@ export type RequestListener = (
 	res: HttpResponse,
 ) => Promise<Response | void> | Response | void;
 
-interface Route {
+/**
+ * Interface pour définir une route.
+ * @interface Route
+ * @property {string} url - L'URL de la route.
+ * @property {HttpMethods} method - La méthode HTTP de la route.
+ * @property {RequestListener[]} middlewares - Les middlewares à appliquer à la route.
+ * @property {RequestListener} requestListener - Le listener à appeler lorsque la route est appelée.
+ */
+export interface Route {
 	url: string;
 	method: HttpMethods;
 	middlewares: RequestListener[];
