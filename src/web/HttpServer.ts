44b1d539262ba404ab3175726ce256abc0f37be5
8borane8 <74107604+8borane8@users.noreply.github.com>
2024-06-26 17:19:14 +0200

    fix

--- src/web/HttpServer.ts
+++ src/web/HttpServer.ts
@@ -21,7 +21,13 @@ export enum HttpMethods {
 	DELETE = "DELETE",
 }
 
-type RequestListener = (
+/**
+ * Le type RequestListener représente la signature d'une fonction qui gère une requête HTTP entrante.
+ * @param {HttpRequest} req - L'objet HttpRequest représentant la requête entrante.
+ * @param {HttpResponse} res - L'objet HttpResponse permettant de définir la réponse HTTP à envoyer.
+ * @returns {Promise<Response | void> | Response | void} Une promesse résolue avec la réponse HTTP à envoyer, ou rien si la réponse a déjà été envoyée.
+ */
+export type RequestListener = (
 	req: HttpRequest,
 	res: HttpResponse,
 ) => Promise<Response | void> | Response | void;
